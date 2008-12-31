f5cf44aa8ecd2e36d5a6f89e39b94db4853e8004
unknown <dmreiland@unixsherpa.com>
2008-12-31 02:07:14 -0600

    Added base theme content from Defusion theme.

---
+++ footer.php
@@ -0,0 +1,19 @@
+<div id="footer">
+	
+	<div id="legal">
+		<p>Copyright &copy; 2008 <a href="<? bloginfo('url'); ?>"><? bloginfo('name'); ?></a></p>
+		<p>Powered by <a href="http://wordpress.org">WordPress</a></p>
+	</div>
+		
+	<div id="webdemar">
+		<p><a href="http://webdemar.com">WordPress Themes</a> by webdemar</p>
+	</div>
+		
+	<div class="clear"></div>
+	
+</div><!-- end footer -->
+
+</div><!-- end page -->
+
+</body>
+</html>
